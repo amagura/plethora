c652a4c8d4cbfa50846a0ab11f27171c2ffdc317
Alexej Magura <agm2819@gmail.com>
2020-01-08 22:23:34 -0700

    added more command-line options--the parser is terrible, I`ll need to write a c frontend

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from os import listdir, getcwd
+from os import listdir, getcwd, environ
 from os.path import isfile, join
 from gi.repository import Gio
 from time import sleep
+from shutil import which
 import random
 import sys
 import subprocess
@@ -10,47 +11,44 @@ import getopt
 version = '0.0.1'
 
 def change_background(filename):
-     # gsettings = Gio.Settings.new('org.gnome.desktop.background')
-     # print(gsettings.get_string('picture-uri'))
-     # gsettings.set_string('picture-uri', 'file://%s' % filename)
-     # gsettings.apply()
-     # print(gsettings.get_string('picture-uri'))
-     subprocess.run(["feh", "--bg-fill", filename])
 
-def getfiles(path):
+     if environ['DESKTOP_SESSION'] in ['gnome', 'unity']:
+          # using gsettings is not ideal
+          # it's incapable of downscaling larger wallpapers
+          # therefore, we'll need to.
+          gsettings = Gio.Settings.new('org.gnome.desktop.background')
+
+          # zoom is the best setting
+          gsettings.set_string('picture-options', 'zoom')
+
+          # time to actually set the background
+          print(gsettings.get_string('picture-uri'))
+          gsettings.set_string('picture-uri', 'file://%s' % filename)
+          gsettings.apply()
+          print(gsettings.get_string('picture-uri'))
+     else:
+          if which('feh') is not None:
+               subprocess.run(["feh", "--bg-fill", filename])
+          else:
+               # TODO:
+               # Write a C/Cython backend that will (using imlib2) set the desktop background using a fill option similar to Feh's fill
+               pass
+
+def getfiles(path, recurse=False):
+     if recurse:
+          f = []
+          (_, _, filenames) = walk(path).next()
+          print(filenames)
+          return f
      return [f for f in listdir(path) if isfile(join(path, f))]
 
 def main():
-     last20 = []
-     while True:
-          # get random file
-          file = random.choice(getfiles(sys.argv[1]))
+     recurse = False
+     duration = 60 * 10 # 10 minutes
 
-          if len(last20) > 19:
-               last20.pop()
-
-          if file in last20:
-               continue
-          else:
-               last20 = [file] + last20
-
-          change_background('%s/%s' % (sys.argv[1], file))
-          sleep(10)
-
-
-def usage():
-     print(
-"""Usage: %s [OPTION]... PATH
-
-  -h, --help			print this message and exit
-  -v, --version		print program version and exit
-  -R, -r, --recursive 	recurse into subdirectories of PATH
-""")
-
-if __name__ == '__main__':
      # Usage: plethora [OPTION] PATH
-     sopts = 'hvrRt:'
-     lopts = ['help', 'version', 'recursive', 'duration:']
+     sopts = 'hvrRs:'
+     lopts = ['help', 'version', 'recursive', 'sleep:']
      try:
           opts, args = getopt.gnu_getopt(sys.argv[1:], sopts, lopts)
      except:
@@ -65,8 +63,41 @@ if __name__ == '__main__':
           elif o in '-v':
                print(version)
                sys.exit()
+          elif o in ('-r', '-R', '--recursive'):
+               recurse = True
+          elif o in ('-s', '--sleep'):
+               duration = a
 
      if len(sys.argv) < 2:
                print('%s: missing directory operand' % sys.argv[0])
                sys.exit(1)
+
+     last20 = []
+     while True:
+          # get random file
+          file = random.choice(getfiles(sys.argv[1]))
+
+          if len(last20) > 19:
+               last20.pop()
+
+          if file in last20:
+               continue
+          else:
+               last20 = [file] + last20
+
+          change_background('%s/%s' % (sys.argv[1], file), recurse)
+          sleep(duration)
+
+
+def usage():
+     print(
+"""Usage: %s [OPTION]... PATH
+
+  -h, --help			print this message and exit
+  -v, --version		print program version and exit
+  -R, -r, --recursive	recurse into subdirectories of PATH
+  -s, --sleep=SECONDS	sleep for SECONDS before changing wallpaper
+""")
+
+if __name__ == '__main__':
      main()
